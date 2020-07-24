a07255ab67100df8c47e2f4216994a274482bcb7
DohyunKIm <rlaehgus1229@gmail.com>
2020-07-24 12:09:42 +0900

    main.c 코드 작성

--- project/main.c
+++ project/main.c
@@ -1,32 +1,40 @@
 #include <stdio.h>
-
+#include <time.h>
 #include <string.h>
-int main() {
+
+struct SWBlock {
+	char blocInfo[4][100];
+	int resCount;
+	time_t startTime;
+	char res[100];	
+};
+
+struct SWBlock blocks[10];
+
+int main()
+{
 	FILE *fp ;
 
-        int i;
+        int i=0;
+	int j=0;
 	char bloc[100];
 	char par[100];
-
-
 	
         fp = fopen("text.txt", "r");
 
-	while(fgets(bloc, 100, fp)) {	
-		
-
-
+	while(fgets(bloc, 100, fp))
+	{		
+		char *ptr = strtok(bloc, " ;");
 	
-	char *ptr = strtok(bloc, " ;");
-	
-	while(ptr != NULL) {
-	printf("%s\n", ptr);
-	ptr = strtok(NULL, " ;");
-}
-
-}
-
+		while(ptr != NULL)
+		{
+			printf("%s\n", ptr);
+			ptr = strtok(NULL, " ;");
+			strcpy(blocks[i].blocInfo[j], ptr); 	
+			j++;
+		}
+		i++;
+	}
         fclose(fp);
-
 	return 0;
 }
