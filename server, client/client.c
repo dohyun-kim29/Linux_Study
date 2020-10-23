9819a2a5222c65cc9c76fcc6d7aaab46a27c15af
DohyunKIm <rlaehgus1229@gmail.com>
2020-10-23 11:54:35 +0900

    [Modify] Client.c

--- server, client/client.c
+++ server, client/client.c
@@ -51,7 +51,53 @@ void *recvMsg(void *params) {
     char msg[MAX_SIZE];
     while(1) {
         memset(msg, 0, MAX_SIZE);
-        int ret = recvfrom(sockfd, )
+        int ret = recvfrom(sockfd, &msg, MAX_SIZE, 0, (struct sockaddr *)&sock, &sock_len);
+        printf("%d msg received\n", ret);
+        if(ret<0)
+        {
+            extern int errno;
+            printf("ret(%d), errno(%d. %s)\n", ret, errno, strerror(errno));
+        }
+        else
+        {
+            printf("%d msg received\n", ret);
+            printf("recv from : %s\n", msg);
+            sock.sin_port +=1;
+        }
+        close(sockfd);
+    }
+
+    int sendMsg2(char *msg, int size, struct sockaddr* sock, int sock_len)
+    {
+        struct sockaddr_in mySock = {AF_INET, MY_PORT_S, INADDR_ANY};
+        int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+        int ret = bind(sockfd, (struct sockaddr *)&mySock, sizeof(mysock));
+        if (ret<0)
+        {
+            extern int errno;
+            printf("ret(%d), errno(%d. %s)\n", ret, errno, strerror(errno));
+        }
+
+        sendto(sockfd, msg, size, 0, sock, sock_len);
+        close(sockfd);
+    }
+
+    int sendMsg(char *msg, int size, char *ip, int port)
+    {
+        struct sockaddr_in mySock = {AF_INET, MY_PORT_S, INADDR_ANY};
+        int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+        int ret = bind(sockfd, (struct sockaddr *)&mySock, sizeof(mySock));
+        if(ret<0)
+        {
+            extern int errno;
+            printf("ret(%d), errno(%d. %s)\n", ret, errno, strerror(errno));
+        }
+
+        struct sockaddr_in sock = {AF_INET, port, INADDR_ANY};
+        sock.sin_addr.s_addr = inet_addr(ip);
+
+        sendto(sockfd, msg, size, 0, (struct sockaddr *)&sock, sizeof(sock));
+        close(sockfd);
     }
     
     
